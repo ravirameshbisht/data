2198db5a6ada8a4cc2a142ce4387a5fccbbf5643
Ravi Bisht <ravirameshbisht@gmail.com>
2025-11-22 19:52:15 +0530

    Add files via upload

--- code/JAVA_BASICS_001_Explaination.java
+++ code/JAVA_BASICS_001_Explaination.java
@@ -1,1 +1,4 @@
-THis is explaination file
+This Java code defines a Runnable task using a concise lambda expression, 
+creates a new Thread with that task, and immediately starts the thread's execution. 
+The lambda's body, System.out.println("Hello, Lambda!");, is executed concurrently in the new thread.
+
