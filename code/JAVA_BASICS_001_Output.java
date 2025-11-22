7b05665aff9b2ce13e4c4a35139da2bf6baf00ae
Ravi Bisht <ravirameshbisht@gmail.com>
2025-11-22 17:45:38 +0530

    Add files via upload

---
+++ code/JAVA_BASICS_001_Output.java
@@ -0,0 +1,28 @@
+Number: 10
+Pi: 3.14159
+Language: Java
+Is Java fun? true
+Sum: 15
+Product: 20
+Number is greater than 5
+
+For Loop:
+i = 1
+i = 2
+i = 3
+i = 4
+i = 5
+
+While Loop:
+j = 1
+j = 2
+j = 3
+j = 4
+j = 5
+
+Array elements:
+1 2 3 4 5 
+
+Sum using method: 15
+
+Student Name: Alice, Age: 21
