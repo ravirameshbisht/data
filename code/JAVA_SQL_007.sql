2b32b391ff9e54acd4f8cb89bf95aa428b131783
Ravi Bisht <fabulousroutine@gmail.com>
2025-12-12 21:24:27 +0530

    Update JAVA_SQL_007.sql

--- code/JAVA_SQL_007.sql
+++ code/JAVA_SQL_007.sql
@@ -1,14 +1,5 @@
 {
-    "id": 7,
-    "name": "Write a query to find the median salary",
-    "query": 'SELECT AVG(salary) AS median_salary
-                FROM (
-                SELECT salary
-                FROM employees
-                ORDER BY salary
-                LIMIT 2 - (SELECT COUNT(*) FROM employees)
-                % 2
-                OFFSET (SELECT (COUNT(*) - 1) / 2 FROM
-                employees)
-                ) AS median_subquery;'
+  "id": 7,
+  "name": "Write a query to find the median salary",
+  "query": "SELECT AVG(salary) AS median_salary\nFROM (\n    SELECT salary\n    FROM employees\n    ORDER BY salary\n    LIMIT 2 - (SELECT COUNT(*) FROM employees) % 2\n    OFFSET (SELECT (COUNT(*) - 1) / 2 FROM employees)\n) AS median_subquery;"
 }
